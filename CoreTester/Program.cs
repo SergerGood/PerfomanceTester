bad50d4816d57343ff6e4f17fde2facdbedb16b3
SergerGood <serzher@inbox.ru>
2018-04-28 10:19:00 +0300

    1

---
+++ CoreTester/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoreTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ FrameworkTester471/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrameworkTester471
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
